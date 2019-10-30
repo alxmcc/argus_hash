f1fbf66c593220eb56c098aae19a76cb4d0351eb
Alex McCrory <20491449+alxmcc@users.noreply.github.com>
2019-10-30 02:21:00 +0000

    add timestamp, change to csv output

--- argus_hash.py
+++ argus_hash.py
@@ -7,6 +7,7 @@ author: github.com/alxmcc
 import hashlib
 import re
 import argparse
+from datetime import datetime as dt
 
 
 def match_header(file_header, regex_header):
@@ -19,6 +20,10 @@ def md5_hash(content):
     return m.hexdigest()
 
 
+def get_timeUTC(time_bytes):
+    return str(dt.fromtimestamp(eval('0x' + time_bytes.hex()))) + ' UTC'
+
+
 def main():
     parser = argparse.ArgumentParser(description="Excludes Argus header/footer information and hashes content.")
     parser.add_argument('input', nargs='+', help='Input filename(s).')
@@ -27,7 +32,7 @@ def main():
     content = b''  # set empty for footer check
     argus_header = re.compile(b'\x83\x10\x00\x20\x00{4}\xE5\x71\x2D\xCB[\x00-\xFF]{112}\xFF{4}')
     argus_footer = re.compile(b'\x83\x30\x00\x20\x00{20}[\x00-\xFF]{100}\xFF{4}')
-    
+
     for argus_file in args.input:
         try:
             with open(argus_file, 'rb') as f:
@@ -41,19 +46,20 @@ def main():
                             content = head_slice[:footer.start()]
                         break  # only want first header match and last footer match
                     if content:
-                            argus_dict[argus_file] = [full_hash, md5_hash(content)]
-                            content = b'' 
+                        creation_time = get_timeUTC(data[24:28])
+                        argus_dict[argus_file] = [creation_time, full_hash, md5_hash(content)]
+                        content = b''
                     else:
                         print(argus_file, 'has header, but no footer. Skipped.')
                 else:
-                    print(argus_file, 'is not a Argus binary. Skipping.')                    
+                    print(argus_file, 'is not a Argus binary. Skipping.')
         except (IsADirectoryError, FileNotFoundError, IOError):
             print(argus_file, 'not a valid file or file not found. Skipping.')
             continue
     if argus_dict:
-        print('\n{: <44} {: <34} {}'.format('Filename', 'Full MD5', 'Content MD5'))
+        print('{},{},{},{}'.format('Filename', 'Argus Creation Time', 'Full MD5', 'Content MD5'))
         for k, v in argus_dict.items():
-            print('{: <44} {: <34} {}'.format(k, v[0], v[1]))
+            print('{},{},{},{}'.format(k, v[0], v[1], v[2]))
     else:
         print('No Argus binary files found.')
 
